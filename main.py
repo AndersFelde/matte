6138cb6cfbd8defffe0e481869f6cf60bf922229
Anders Kvamme Felde <anderskfelde@gmail.com>
2020-11-19 12:52:28 +0100

    bug fixing

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import sympy
 
 class CalcDistance():
     def __init__(self, A, B, C, point):
-        self.A = set(A)
-        self.B = set(B)
-        self.C = set(C)
+        self.A = list(A)
+        self.B = list(B)
+        self.C = list(C)
         self.point = point
 
         self.alpha = Plan(A, B, C)
@@ -15,7 +15,7 @@ class CalcDistance():
     def distancePointToPlan(self, point, plan):
         r = plan.retningsVektor
         absOfPlan = abs(r[0]*point[0]+r[1]*point[1]+r[2]*point[2])
-        lengthOfRetning = math.sqrt(r[0]**2+r[1]**2+r[2**2])
+        lengthOfRetning = math.sqrt(r[0]**2+r[1]**2+r[2]**2)
         return absOfPlan/lengthOfRetning
 
     def movePoint(self, point, direction, distance):
@@ -23,11 +23,8 @@ class CalcDistance():
         movingVector = [direction[0]*t, direction[1]*t, direction[2]*t]
 
         movedPoint = (
-            point + movingVector[0], point + movingVector[1], point + movingVector[2])
+            point[0] + movingVector[0], point[1] + movingVector[1], point[2] + movingVector[2])
         return movedPoint
-    
-    def isBetweenPoints(self, A, B):
-        pass
 
     def checkCrossingLine(self, lineA, lineB):
         # [A.A[0]+A.r[0]*t, A.A[1]+A.r[1]*t, A.A[2]+A.r[2]*t] = [B.A[0]+B.r[0]*s, B.A[1]+B.r[1]*s, B.A[2]+B.r[2]*s]
@@ -36,28 +33,88 @@ class CalcDistance():
         # yu
         A = lineA
         B = lineB
+        print(A.A[0] + A.r[0] - B.A[0] - B.r[0])
+        print(type(A.A[0]))
         s, t = sympy.symbols("s t")
+
         eq1 = sympy.Eq(A.A[0] + A.r[0]*t - B.A[0] - B.r[0]*s, 0)
         eq2 = sympy.Eq(A.A[1] + A.r[1]*t - B.A[1] - B.r[1]*s, 0)
 
         sol = sympy.solve((eq1, eq2), (s, t))
-        t = sol[t]
+        print(sol)
+        for key in sol:
+            t = sol[key]
 
         return (A.A[0] + A.r[0]*t, A.A[1] + A.r[1]*t, A.A[2] + A.r[2]*t)
 
+    def isInsideTriangle(self, A, B, C):
+        lineAB = Line(A, B)
+        lineBC = Line(B, C)
+        lineAC = Line(A, C)
+
+        sides = [lineAB, lineBC, lineAC]
+
+        lineAP = Line(A, self.point)
+
+        crossings = 0
+
+        for line in sides:
+            crossPoint = self.checkCrossingLine(lineAP, line)
+            if isBetweenPoints(line.A, line.B, crossPoint):
+                crossings += 1
+
+        if crossings == 2:
+            return True
+
+        return False
+
+    def calcDistance(self, A, B, C):
+        self.distances = []
+
+        lineAB = Line(A, B)
+        lineBC = Line(B, C)
+        lineAC = Line(A, C)
+
+        sides = [lineAB, lineBC, lineAC]
+
+        for line in sides:
+            crossingPoint = line.closestPointOnLine(self.point)
+            if isBetweenPoints(A, B, crossingPoint):
+                self.distances.append(
+                    distanceBetweenPoints(self.point, crossingPoint))
+            else:
+                distanceA = distanceBetweenPoints(line.A, self.point)
+                distanceB = distanceBetweenPoints(line.B, self.point)
+
+                if distanceB < distanceA:
+                    self.distances.append(distanceB)
+                else:
+                    self.distances.append(distanceA)
+
+        self.distances.sort()
+
+        xDistance = self.distances[0]
+        yDistance = self.distancePointToPlan(self.point, self.alpha)
+
+        distance = math.sqrt(xDistance**2 + yDistance**2)
+
+        return distance
 
 
 class Plan():
     def __init__(self, A, B, C):
-        self.A = set(A)
-        self.B = set(B)
-        self.C = set(C)
+        self.A = list(A)
+        self.B = list(B)
+        self.C = list(C)
 
         self.AB = vektorMellomPunkt(self.A, self.B)
         self.AC = vektorMellomPunkt(self.A, self.C)
         self.BC = vektorMellomPunkt(self.B, self.C)
 
-        self.retningsVektor = np.cross(self.AB, self.AC)
+        self.numpyRetningsVektor = np.cross(self.AB, self.AC)
+        self.retningsVektor = []
+        for x in self.numpyRetningsVektor:
+            self.retningsVektor.append(int(x))
 
 
 class Line():
@@ -67,7 +124,7 @@ class Line():
 
         self.r = vektorMellomPunkt(A, B)
 
-    def pointOnLine(self, point):
+    def closestPointOnLine(self, point):
         A = point
         r = self.r
 
@@ -91,6 +148,22 @@ def vektorMellomPunkt(a, b):
     return [b[0]-a[0], b[1]-a[1], b[2]-a[2]]
 
 
+def isBetweenPoints(a, b, point):
+    lengthAP = distanceBetweenPoints(a, point)
+    lengthBP = distanceBetweenPoints(b, point)
+
+    lengthAB = distanceBetweenPoints(a, b)
+
+    if lengthAP <= lengthAB and lengthBP <= lengthAB:
+        return True
+
+    return False
+
+
+def distanceBetweenPoints(a, b):
+    return math.sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2 + (a[2]-b[2])**2)
+
+
 def main():
     punkt = (5, 6, 7)
     A = (1, 2, 3)
@@ -105,3 +178,15 @@ def main():
     nyA = calc.movePoint(A, alpha.retningsVektor, distansePunktTilAlpha)
     nyB = calc.movePoint(B, alpha.retningsVektor, distansePunktTilAlpha)
     nyC = calc.movePoint(C, alpha.retningsVektor, distansePunktTilAlpha)
+
+    if calc.isInsideTriangle(nyA, nyB, nyC):
+        print(calc.distancePointToPlan(punkt, alpha))
+        return
+
+    distance = calc.calcDistance(nyA, nyB, nyC)
+    print(distance)
+    return
+
+
+if __name__ == "__main__":
+    main()
